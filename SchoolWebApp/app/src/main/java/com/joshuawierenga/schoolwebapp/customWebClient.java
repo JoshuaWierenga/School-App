0e4c1d96599388926d3b720a781f30963c661855
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2016-06-28 23:19:37 +1000

    offline mode
    trying to display text to says its in offline mode

--- SchoolWebApp/app/src/main/java/com/joshuawierenga/schoolwebapp/customWebClient.java
+++ SchoolWebApp/app/src/main/java/com/joshuawierenga/schoolwebapp/customWebClient.java
@@ -8,7 +8,8 @@ import android.webkit.WebViewClient;
 public class customWebClient extends WebViewClient {
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if (Uri.parse(url).getHost().equals("newtownhighschooltas.org")) {
+        String site = Uri.parse(url).getHost();
+        if (site.equals("newtownhighschooltas.org") || site.equals("newtownhighschooltasmania.wordpress.com")) {
             return false;
         }
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
--- SchoolWebApp/app/src/main/java/com/joshuawierenga/schoolwebapp/web.java
+++ SchoolWebApp/app/src/main/java/com/joshuawierenga/schoolwebapp/web.java
@@ -1,5 +1,7 @@
 package com.joshuawierenga.schoolwebapp;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
@@ -17,8 +19,26 @@ public class web extends AppCompatActivity {
 
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        if (!isNetworkAvalable()) {
+            webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+            //String offlinetext = "<html><body>Internet Lost: Showing Offline Version</body></html>";
+            webView.loadUrl("javascript:function offline() {var div = Document.getElementsByClassName('navbar-header')[0], div:innerHTML += \"<p style=\"text-align:justify;\"><strong>Internet Lost: Showing Offline Version</strong></p>\"} offline()");
+
+
+        }
 
         webView.loadUrl("https://newtownhighschooltas.org/");
 
+    }
+
+    private boolean isNetworkAvalable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
