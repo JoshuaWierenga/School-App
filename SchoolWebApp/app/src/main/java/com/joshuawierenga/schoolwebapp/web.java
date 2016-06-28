4de1852e62a730180a0affd938fbaabab78d3b6d
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2016-06-28 20:29:42 +1000

    Added WebView Activity

---
+++ SchoolWebApp/app/src/main/java/com/joshuawierenga/schoolwebapp/web.java
@@ -0,0 +1,23 @@
+package com.joshuawierenga.schoolwebapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class web extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://newtownhighschooltas.org/");
+
+    }
+}
